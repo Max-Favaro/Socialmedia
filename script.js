4333da2145e27923352b6f6e8eea8be86aefcb66
Maxwel Fávaro <83313519+Max-Favaro@users.noreply.github.com>
2021-12-31 14:53:54 -0300

    My fist clone page
    Project D

---
+++ script.js
@@ -0,0 +1,10 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var closeimgads = document.querySelector(".link-ads");
+
+function SettingsMenuToggle() {
+    settingsmenu.classList.toggle("settings-menu-height");
+}
+
+function Closeads() {
+    closeimgads.style.display = "none";
+}
